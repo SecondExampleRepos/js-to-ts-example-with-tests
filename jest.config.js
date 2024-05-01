7063a0850f61571cd3029b5e2b43e014c862c254
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-04-30 22:55:19 -0700

    edit_file

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 /**
  * @file Jest configuration.
  */
-
-module.exports = {
+type JestConfig = {
+  rootDir: string;
+  testRegex: string;
+  setupFiles: string[];
+const config: JestConfig = {
   rootDir: 'src/test',
   testRegex: '/src/test/.*test\\.js$',
   setupFiles: ['<rootDir>/setup.js'],
-};
+export default config;

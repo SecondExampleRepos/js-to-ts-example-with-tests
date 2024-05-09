92731f9a402f3daa58039be17d20ed5aa3669880
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 10:54:01 +0200

    edit_file

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,9 +1,7 @@
 /**
  * @file Tests setup.
  */
-
 import '@babel/polyfill';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({ adapter: new Adapter() });

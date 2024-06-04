c1840c7e449df1c2afb2bf54dc601448ba43970d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-04 19:20:22 +0000

    edit_file

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 /**
  * @file Jest configuration.
  */
-
-module.exports = {
+import { Config } from '@jest/types';
+const config: Config.InitialOptions = {
   rootDir: 'src/test',
-  testRegex: '/src/test/.*test\\.js$',
-  setupFiles: ['<rootDir>/setup.js'],
+  testRegex: '/src/test/.*test\\.ts$',
+  setupFiles: ['<rootDir>/setup.ts'],
 };
+export default config;
